f938213aa34bbc5fa77658da8da926b57c522ece
Aleksey Petrunnik <petrunnik.a@gmail.com>
2023-06-24 16:35:35 +0300

    Add authentication support for the /webhook/grafana/ handler
    - update deps
    - implement grafana request handling without message

--- src/alert_manager/config.py
+++ src/alert_manager/config.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import typing as t
 from enum import Enum
 from pathlib import Path
 
-from pydantic import Field
+from pydantic import Field, Json, validator
 from pydantic.env_settings import BaseSettings
 
 _inst: dict[str, Config] = {}
@@ -34,6 +35,7 @@ class Config(BaseSettings):
     log_format: LogMode = LogMode.simple
     log_level: LogLevel = LogLevel.info
     filter_backend: FilterBackend = Field(default=FilterBackend.in_memory)
+    accounts_: Json[t.Any] | None = Field(env='ACCOUNTS')
 
     # sentry
     sentry_dsn: str | None
@@ -46,12 +48,33 @@ class Config(BaseSettings):
     # redis
     redis_url: str | None
 
+    @validator('accounts_')
+    def accounts_validator(
+        cls, value: t.Any, **kwargs: t.Any  # noqa: N805
+    ) -> dict[str, str] | None:
+        if value is None:
+            return None
+        if not isinstance(value, dict):
+            raise ValueError('Must be a dict')
+        if not all(
+            all((isinstance(key, str), isinstance(value, str))) for key, value in value.items()
+        ):
+            raise ValueError('Key and value in dict must be string')
+        return value
 
-def get_config(env_file: Path = project_dir / '.env') -> Config:
+    @property
+    def accounts(self) -> dict[str, str] | None:
+        return self.accounts_
+
+    class Config:
+        env_file = project_dir / '.env'
+
+
+def get_config() -> Config:
     try:
         config = _inst['conf']
     except KeyError:
-        config = Config(_env_file=env_file)  # type: ignore[call-arg]
+        config = Config()  # type: ignore[call-arg]
         _inst['conf'] = config
 
     return config
