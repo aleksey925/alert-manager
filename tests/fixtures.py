323e2f92a5148397d3549f1cef07bc99cd74bbbe
Aleksey Petrunnik <petrunnik.a@gmail.com>
2023-07-22 15:35:31 +0300

    Add snooze for 8 hours, 2 days
    - refactoring tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,7 +3,12 @@ from pathlib import Path
 
 import pytest
 
-__all__ = ['legacy_alert_alerting', 'legacy_alert_no_data', 'legacy_alert_ok']
+__all__ = [
+    'legacy_alert_alerting',
+    'legacy_alert_no_data',
+    'legacy_alert_no_data_alerting',
+    'legacy_alert_ok',
+]
 
 
 @pytest.fixture
@@ -14,6 +19,11 @@ def legacy_alert_alerting(test_dir: Path):
 @pytest.fixture
 def legacy_alert_no_data(test_dir: Path):
     return json.loads((test_dir / 'data' / 'legacy_alerts.json').read_text())['no_data']
+
+
+@pytest.fixture
+def legacy_alert_no_data_alerting(test_dir: Path):
+    return json.loads((test_dir / 'data' / 'legacy_alerts.json').read_text())['no_data_alerting']
 
 
 @pytest.fixture
